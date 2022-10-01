f557f2a81fb6f0260d1a666aff7d51837f0d0218
Mustafa Barodawala <24886321+itsjustmustafa@users.noreply.github.com>
2022-10-01 21:52:48 +1000

    ADDED: main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import ffmpy
+
+title_art = """
+     .88b  d88. d88888b d8888b. d888888b  .d8b.  
+     88'YbdP`88 88'     88  `8D   `88'   d8' `8b 
+     88  88  88 88ooooo 88   88    88    88ooo88 
+     88  88  88 88~~~~~ 88   88    88    88~~~88 
+     88  88  88 88.     88  .8D   .88.   88   88 
+     YP  YP  YP Y88888P Y8888D' Y888888P YP   YP 
+                                                 
+                                                 
+.d8888.  .d88b.  d8888b.  .o88b. d88888b d8888b. 
+88'  YP .8P  Y8. 88  `8D d8P  Y8 88'     88  `8D 
+`8bo.   88    88 88oobY' 8P      88ooooo 88oobY' 
+  `Y8b. 88    88 88`8b   8b      88~~~~~ 88`8b   
+db   8D `8b  d8' 88 `88. Y8b  d8 88.     88 `88. 
+`8888Y'  `Y88P'  88   YD  `Y88P' Y88888P 88   YD 
+"""
+
+def main():
+    # Cool comment
+    print(title_art)
+
+if __name__ == "__main__":
+    main()
